b06af4ba2927c1b7c48a029349605e3377bce464
SysBioUAB <78474998+SysBioUAB@users.noreply.github.com>
2024-05-06 23:50:18 +0200

    Update align2D.py

--- align2D.py
+++ align2D.py
@@ -2,8 +2,8 @@ from modeller import *
 
 env = environ()
 aln = alignment(env)
-mdl = model(env, file='2hky', model_segment=('FIRST:A','LAST:A'))
-aln.append_model(mdl, align_codes='2hkyA', atom_files='2hky.pdb')
+mdl = model(env, file='6enp', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='6enpA', atom_files='6enp.pdb')
 aln.append(file='target.fasta', align_codes='target', alignment_format='FASTA')
 aln.align2d()
 aln.write(file='aligned.fasta', alignment_format='FASTA')
